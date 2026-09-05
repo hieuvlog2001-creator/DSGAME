16a21246c447883654cd0b02da4e88b67e0a379b
hieuvlog2001-creator <hieuvlog2001@gmail.com>
2026-09-05 21:33:05 +0700

    Change ADMIN_PASSWORD in admin-config.js
    Updated the admin login password for security.

--- Admin/admin-config.js
+++ Admin/admin-config.js
@@ -1,5 +1,5 @@
 // DS Games Admin - public login configuration
 // NOTE: This file is public on GitHub Pages. Do NOT put GITHUB_TOKEN here.
 // Change ADMIN_PASSWORD here to change the Admin login password.
-const ADMIN_PASSWORD = 'DSGAMES-ADMIN-2026';
+const ADMIN_PASSWORD = 'hieudn95';
 const ADMIN_WORKER_URL = 'https://dsgame.hieuvlog2001.workers.dev';
